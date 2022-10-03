4523772c40d67e92af772bef243a678b27c506a0
imusa007 <irajibdu@gmail.com>
2022-10-03 11:26:18 -0400

    Mtn: swap_atom raised in mcdft namespace

--- python/mcdft/__init__.py
+++ python/mcdft/__init__.py
@@ -1,11 +1,1 @@
-from mcdftstructure._mcdftstructure import add_one
-
-# Export the version given in project metadata
-from importlib import metadata
-
-__version__ = metadata.version(__package__)
-del metadata
-
-
-def one_plus_one():
-    return add_one(1)
+from structure_generator import swap_atoms
