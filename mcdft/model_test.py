8add7f5315f066315f236e10e3ee558564bcb549
imusa007 <irajibdu@gmail.com>
2023-02-08 15:10:22 -0500

    Add ml model test functionality

---
+++ mcdft/model_test.py
@@ -0,0 +1,24 @@
+from sklearn.metrics import r2_score
+import numpy as np
+import time
+
+
+class ML_tester:
+    def __init__(self, trainer, out_filename):
+        self.trainer = trainer
+
+    def test(self):
+        for i in range(self.trainer.train_size, len(self.trainer.all_clusters)):
+            t1 = time.time()
+            new_cluster = self.trainer.all_clusters[i]
+            pred_E = predict_energy(self.trainer, new_cluster)
+            real_E = self.trainer.all_energy[i]
+
+            if pred_E is None:
+                self.trainer.refit(new_cluster, real_E)
+                delta_time = self.trainer.all_time[i]
+                msg = f"{i:5} {real_E:10.5f} {str(np.nan):>11} {str(np.nan):>11} 1 {delta_time:20.3f}"
+            else:
+                delta_time = time.time() - t1
+                err = real_E - pred_E
+                msg = f"{i:5} {real_E:10.5f} {pred_E:10.5f} {err:10.5f} 0 {delta_time:20.3f}"
